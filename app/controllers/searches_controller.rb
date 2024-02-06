5dbd99af783ce3ccdf68fd9bd7d1caef85c9739a
tomoyoshimizu <154865008+tomoyoshimizu@users.noreply.github.com>
2024-02-06 02:51:11 +0000

    [Fix] small refactoring

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,25 +1,19 @@
 class SearchesController < ApplicationController
 
   def search
-    @search_word = search_params[:search_word]
-    @search_model = search_params[:search_model]
+    @search_word = params[:search_word]
+    @search_model = params[:search_model]
 
     case @search_model
     when "User" then
-      referenced_model = User
+      model_class = User
     when "Book" then
-      referenced_model = Book
+      model_class = Book
     end
 
-    @result = referenced_model.search(@search_word, search_params[:search_type])
+    @search_result = model_class.search(@search_word, params[:search_type])
 
     @new_book = Book.new
-  end
-
-  private
-
-  def search_params
-    params.permit(:search_word, :search_model, :search_type)
   end
 
 end
--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -7,24 +7,24 @@ module Searchable
 
       case self.to_s
       when "User" then
-        column_names = "name"
+        target_column = "name"
       when "Book" then
-        column_names = "title"
+        target_column = "title"
       end
 
       case search_type
-      when "部分一致" then
-        search_string = "%#{search_word}%"
-      when "完全一致" then
-        search_string = search_word.to_s
-      when "前方一致" then
-        search_string = "#{search_word}%"
-      when "後方一致" then
-        search_string = "%#{search_word}"
+      when "partial" then
+        search_string = "%" + search_word + "%"
+      when "perfect" then
+        search_string = search_word
+      when "forward" then
+        search_string = search_word + "%"
+      when "backward" then
+        search_string = "%" + search_word
       end
 
       if search_word.present?
-        where("#{column_names} LIKE ?", search_string)
+        where("#{target_column} LIKE ?", search_string)
       else
         all
       end
