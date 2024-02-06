126a820c32f06e12fb790133e1b8aa0954e7b4b7
tomoyoshimizu <154865008+tomoyoshimizu@users.noreply.github.com>
2024-02-06 06:42:11 +0000

    [Fix] integrate view files

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,11 +3,13 @@ class FavoritesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     current_user.favorites.create(book_id: @book.id)
+    render 'render_btn'
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     current_user.favorites.find_by(book_id: @book.id).destroy
+    render 'render_btn'
   end
 
 end
