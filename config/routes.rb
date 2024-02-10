c7b8bd3558638dbe32a1848b42c82aa26219282a
tomoyoshimizu <154865008+tomoyoshimizu@users.noreply.github.com>
2024-02-10 12:26:08 +0000

    [Add] function of search user's posts count at any day

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root :to => "homes#top"
   get "about" => "homes#about", as: "about"
   get "search" => "searches#search"
+  get "search_date" => "searches#search_date"
 
   devise_for :users
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
