ddf856fe30d3c63624f93ed64394db901e5595bc
tomoyoshimizu <154865008+tomoyoshimizu@users.noreply.github.com>
2024-02-12 09:41:21 +0000

    [Add] function of group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_08_054117) do
+ActiveRecord::Schema.define(version: 2024_02_12_071354) do
 
   create_table "access_records", force: :cascade do |t|
     t.integer "book_id"
@@ -75,6 +75,21 @@ ActiveRecord::Schema.define(version: 2024_02_08_054117) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "group_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.integer "owner_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
